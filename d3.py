d9e4aeab13583b3803931391b705473c6dcbb1db
Aman-sharma077 <amanaharma.1712@gmail.com>
2024-05-14 14:09:35 +0530

    day3 code uploded

---
+++ d3.py
@@ -0,0 +1,76 @@
+# lst =[12,23,56,'hello',True]
+# print(lst)
+# print(lst[1])
+# print(lst[1:4])
+# lst[1] = 500
+# print(lst)
+ 
+# lst.append('hlo')
+
+
+# lst.pop()
+# print(lst)
+
+# lst.insert(1,'kkk')
+# print(lst)
+
+#print(lst[3][-1])
+
+# mess = lst[3]
+# print(mess[-1])
+# lst = [12,244,56.7,[100,200,300]]
+# print(lst[3][1])
+# lst[3][1]=1000
+# # print(lst)
+# tpl = (12,23,44,56,'jecrc',True)
+# tpl[1]=200
+# print(tpl)
+# print(type(tpl))
+# clg_name=('jecrc',)
+
+# print(type(clg_name))
+
+# clg_name="ssd","ssc",'ffj'
+# clg = "jjk",
+# print(type(clg_name))
+# lst = [10,23,56,78,54,34]
+# print(lst)
+# lst.remove(56)
+# print(lst)
+
+# name='harsh '
+# surname='saini '
+# roll=72
+
+# print(name+surname)
+
+# print(name+surname*5)
+
+# print(name+roll)
+
+# tpl2= (12,45.5,5)
+
+# print(tpl2*2)
+
+# lst=(56,89,65,"hlo")
+
+# print(lst*1000000)
+
+# lst1=[1,2,3,4]
+# lst2=[6,7,8,9]
+
+# # lst1.append(lst2)
+# lst1.extend(lst2)
+# print(lst1)
+
+#key,values
+
+# dt = {'name':'harsh',"jaat":"saini","roll":72}
+#print(type(dt))
+# print(dt["name"])
+# print(dt.keys())
+# print(dt.values())
+
+dt={'name':['a','s','d'], 'marks':['1','2','3']}
+
+print(dt['name'])
