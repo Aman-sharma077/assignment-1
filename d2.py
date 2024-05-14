0b09a26bfc22c9f7cab0df2da0a3d79ad86cf731
Aman-sharma077 <amanaharma.1712@gmail.com>
2024-05-14 15:01:27 +0530

    d2

---
+++ d2.py
@@ -0,0 +1,37 @@
+
+
+var1 = 10
+var2 = 36.8
+var3 = True
+var4 = "hgf"
+var5 = 'h'
+var6 = """jecrc"""
+
+#print(type(var2))
+#print(type(var3))
+#print(type(var1))
+#print(type(var4))
+#print(type(var5))
+#print(type(var6))
+
+
+college = "jecrc foundation"
+
+# print(college.endswith('n'))
+
+# print(college.startswith('b'))
+# print(college.strip())
+# print(college.lstrip())
+# print(college.rstrip())
+# j e c r c  f o u n d a t i o n
+#  1 2 3 4 5 6 7 8 9 10
+# print(college[2])
+#
+# print(college[0:4])
+
+# clg='JECRC'
+
+# print(clg[4::-1 ])
+
+clg = 'JECRC jaipur rajasthan'
+print(clg[21::])
